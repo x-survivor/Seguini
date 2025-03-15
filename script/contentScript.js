623da6d312460b47a6d6a4992e43349841b01c8e
xsurvivor <39451941+x-survivor@users.noreply.github.com>
2025-03-16 00:18:24 +0500

    Version 1.0.0 fully functional

--- script/contentScript.js
+++ script/contentScript.js
@@ -1,57 +1,89 @@
 let url = window.location.href;
-let cookie_name = url.split("=")[1];
-let localChromeStorage = chrome.storage.local;
-let video;
+let video = null;
+let key = url.split("=")[1];
+let intervalID = null;
+let source = null;
+let observer = null;
+let URLObserver = null;
 
-// MutationObserver to detect video element
-const observer = new MutationObserver(() => {
+observer = new MutationObserver(() => {
   video = document.querySelector("video");
   if (video) {
+    observer.disconnect();
+    source = video.src;
     url = window.location.href;
-    cookie_name = url.split("=")[1];
+    key = url.split("=")[1];
     initializeVideoState();
   }
 });
 
-// Start observing changes in the DOM
+URLObserver = new MutationObserver(() => {
+  if (video.src !== source) {
+    url = window.location.href;
+    let urlParams = new URLSearchParams(window.location.search);
+    if (urlParams.get("v")) {
+      observer.observe(document.body, { childList: true, subtree: true });
+    }
+  }
+});
+
 observer.observe(document.body, { childList: true, subtree: true });
+URLObserver.observe(document.body, { childList: true, subtree: true });
 
-// Function to initialize video state from cookies
-async function initializeVideoState() {
-  localChromeStorage.get([cookie_name]).then((data)=>{
-    console.log(data);
+function initializeVideoState() {
+  chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
+    if (message.action === "getData" && key != null) {
+      sendResponse({ data: [key], url: url });
+    } else {
+      sendResponse({
+        data: "Invalid Message Request or Key is null",
+      });
+    }
+    return true;
   });
-  // Periodically save video progress
-  setInterval(saveVideoProgress, 10000);
-}
 
-// Function to save video progress in cookies
-function saveVideoProgress() {
-  if (video && !video.paused) {
-    let object = {
-      url: url,
-      currentVideoTime: video.currentTime,
-    };
-    setcookie(cookie_name, JSON.stringify(object));
-    let cookieData = retrieveCookie(cookie_name);
-    console.log(JSON.parse(decodeURIComponent(cookieData)));
-    chrome.storage.local.set({ [cookie_name]: object }).then(() => {});
+  chrome.storage.local.get([key]).then((data) => {
+    if (data[key] && data[key] != null) {
+      video = document.querySelector("video");
+      video.currentTime = data[key]["currentVideoTime"];
+    }
+  });
+  if (!intervalID) {
+    intervalID = setInterval(saveVideoProgress, 10000);
   }
 }
 
-//Send data to Popup script
-function sendData(cookie_name) {
+function saveVideoProgress() {
+  let object = null;
+  let parentNode = document.getElementById("above-the-fold");
+  let title = parentNode.childNodes[1].children[1].innerText;
+  let channel_name =
+    parentNode.children[1].children[0].children[0].children[1].children[0]
+      .innerText;
+  video = document.querySelector("video");
+
+  if (video && !video.paused) {
+    object = {
+      url: url,
+      currentVideoTime: video.currentTime,
+      title: title,
+      channel_name: channel_name,
+    };
+    chrome.storage.local.set({ [key]: object });
+  }
+}
+
+function sendData(key) {
   chrome.runtime.sendMessage({
     action: "sendData",
-    data: cookie_name,
+    data: key,
   });
 }
-// Function to set a cookie
+
 function setcookie(name, value) {
   document.cookie = `${name}=${encodeURIComponent(value)}; path=/;`;
 }
 
-// Function to retrieve a cookie by name
 function retrieveCookie(name) {
   let cookies = document.cookie.split("; ").reduce((acc, cookie) => {
     let [key, value] = cookie.split("=");
--- script/pop.js
+++ script/pop.js
@@ -1,9 +1,72 @@
-let itemContainer = document.getElementById("itemContainer");
-console.log(itemContainer);
-chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
-  if (message.action === "sendData") {
-    console.log("Received in popup:", message.data);
-    itemContainer.innerText = message.data;
-    sendResponse({ success: true });
+const itemsContainer = document.getElementById("itemContainer");
+const currendVideoSave = document.getElementById("currentVideoSave");
+
+chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+  if (tabs.length === 0) return;
+  if (tabs[0].url.includes("://www.youtube.com/watch?v")) {
+    chrome.tabs.sendMessage(tabs[0].id, { action: "getData" }, (response) => {
+      let key = response.data;
+      if (chrome.runtime.lastError) {
+        console.error("Error:", chrome.runtime.lastError.message);
+        return;
+      }
+      chrome.storage.local.get([key][0]).then((result) => {
+        if (result[key[0]]) {
+          currentVideoSave.innerHTML =
+            "<div> <label> " +
+            result[key[0]]["title"] +
+            " " +
+            Math.floor(result[key[0]]["currentVideoTime"] / 60) +
+            ":" +
+            (result[key[0]]["currentVideoTime"] % 60).toFixed(0) +
+            "</label></div><br><hr>";
+        } else {
+          currendVideoSave.innerText = response.data;
+        }
+      });
+    });
+    getAllStoredTimestamps();
+  } else {
+    if (tabs[0].url.includes("://www.youtube.com")) {
+      currendVideoSave.innerText = "Play a Video";
+    } else {
+      currendVideoSave.innerText = "Not a youtube video tab";
+    }
   }
 });
+
+function deleteTime(id) {
+  let articleID = id + "125";
+  let item = document.getElementById(articleID);
+  console.log(id);
+  chrome.storage.local.remove([id], function () {
+    if (item && itemsContainer.contains(item)) {
+      item.style.transform = `translateX(100px)`;
+      item.remove();
+    }
+    return "success";
+  });
+}
+function getAllStoredTimestamps() {
+  chrome.storage.local.get(null).then((data) => {
+    Object.entries(data).forEach(([id, details]) => {
+      let articleID = id + "125";
+      itemsContainer.innerHTML += `<article class=${"item"} id=${articleID}><div>
+        <label class=${"title"}>${
+        details.title
+      }</label> <br> <label class=${"stamp"}> ${Math.floor(
+        details.currentVideoTime / 60
+      )} : ${(details.currentVideoTime % 60).toFixed(
+        0
+      )}</label> </div><div><button class=${"deleteBtn"} id=${id}><svg viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" stroke="#000000"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <path d="M10 11V17" stroke="#ffffff" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path> <path d="M14 11V17" stroke="#ffffff" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path> <path d="M4 7H20" stroke="#ffffff" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path> <path d="M6 7H12H18V18C18 19.6569 16.6569 21 15 21H9C7.34315 21 6 19.6569 6 18V7Z" stroke="#ffffff" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path> <path d="M9 5C9 3.89543 9.89543 3 11 3H13C14.1046 3 15 3.89543 15 5V7H9V5Z" stroke="#ffffff" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path> </g></svg></button></div></article>`;
+    });
+  });
+}
+itemsContainer.addEventListener("click", (event)=>{
+  let deleteBtn = event.target.closest(".deleteBtn");
+  if(deleteBtn){
+    let itemID = deleteBtn.getAttribute("id");
+    deleteTime(itemID);
+  }
+});
+
