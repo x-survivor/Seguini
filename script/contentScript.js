2932c14df9e6e951213cd74f92e2da1713709e15
x-survivor <muhammadasher183@gmail.com>
2025-04-23 09:21:27 +0500

    Changes

--- script/contentScript.js
+++ script/contentScript.js
@@ -1,25 +1,21 @@
-let url = window.location.href;
+let key = getKey();
 let video = null;
-let key = url.split("=")[1];
 let intervalID = null;
 let source = null;
-let observer = null;
-let URLObserver = null;
 
-observer = new MutationObserver(() => {
+let observer = new MutationObserver(() => {
   video = document.querySelector("video");
   if (video) {
     observer.disconnect();
     source = video.src;
-    url = window.location.href;
-    key = url.split("=")[1];
+    key = getKey();
     initializeVideoState();
   }
 });
 
-URLObserver = new MutationObserver(() => {
-  if (video.src !== source) {
-    url = window.location.href;
+let URLObserver = new MutationObserver(() => {
+  video == null ? getVideo() : null;
+  if (video && video.src !== source) {
     let urlParams = new URLSearchParams(window.location.search);
     if (urlParams.get("v")) {
       observer.observe(document.body, { childList: true, subtree: true });
@@ -27,13 +23,17 @@ URLObserver = new MutationObserver(() => {
   }
 });
 
-observer.observe(document.body, { childList: true, subtree: true });
+
+if (window.location.href.includes("://www.youtube.com/watch?v")) {
+  observer.observe(document.body, { childList: true, subtree: true });
+}
+
 URLObserver.observe(document.body, { childList: true, subtree: true });
 
 function initializeVideoState() {
   chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
     if (message.action === "getData" && key != null) {
-      sendResponse({ data: [key], url: url });
+      sendResponse({ data: [key], url: window.location.href });
     } else {
       sendResponse({
         data: "Invalid Message Request or Key is null",
@@ -44,7 +44,7 @@ function initializeVideoState() {
 
   chrome.storage.local.get([key]).then((data) => {
     if (data[key] && data[key] != null) {
-      video = document.querySelector("video");
+      video = getVideo();
       video.currentTime = data[key]["currentVideoTime"];
     }
   });
@@ -54,7 +54,6 @@ function initializeVideoState() {
 }
 
 function saveVideoProgress() {
-  let object = null;
   let parentNode = document.getElementById("above-the-fold");
   let title = parentNode.childNodes[1].children[1].innerText;
   let channel_name =
@@ -63,33 +62,21 @@ function saveVideoProgress() {
   video = document.querySelector("video");
 
   if (video && !video.paused) {
-    object = {
-      url: url,
-      currentVideoTime: video.currentTime,
-      title: title,
-      channel_name: channel_name,
-    };
-    chrome.storage.local.set({ [key]: object });
+    chrome.storage.local.set({
+      [key]: {
+        url: window.location.href,
+        currentVideoTime: video.currentTime,
+        title: title,
+        channel_name: channel_name,
+      },
+    });
   }
 }
 
-function sendData(key) {
-  chrome.runtime.sendMessage({
-    action: "sendData",
-    data: key,
-  });
+function getVideo() {
+  video = document.querySelector("video");
+  return video;
 }
-
-function setcookie(name, value) {
-  document.cookie = `${name}=${encodeURIComponent(value)}; path=/;`;
-}
-
-function retrieveCookie(name) {
-  let cookies = document.cookie.split("; ").reduce((acc, cookie) => {
-    let [key, value] = cookie.split("=");
-    acc[key] = value;
-    return acc;
-  }, {});
-
-  return cookies[name] || null;
+function getKey() {
+  return window.location.href.split("=")[1];
 }
--- script/pop.js
+++ script/pop.js
@@ -3,9 +3,13 @@ const currendVideoSave = document.getElementById("currentVideoSave");
 
 chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
   if (tabs.length === 0) return;
+  if (tabs[0].url.includes("://www.youtube.com")) {
+    getAllStoredTimestamps();
+  }
   if (tabs[0].url.includes("://www.youtube.com/watch?v")) {
     chrome.tabs.sendMessage(tabs[0].id, { action: "getData" }, (response) => {
       let key = response.data;
+      (!key) ? chrome.tabs.reload(tabs[0].id) : null;
       if (chrome.runtime.lastError) {
         console.error("Error:", chrome.runtime.lastError.message);
         return;
@@ -19,13 +23,12 @@ chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
             Math.floor(result[key[0]]["currentVideoTime"] / 60) +
             ":" +
             (result[key[0]]["currentVideoTime"] % 60).toFixed(0) +
-            "</label></div><br><hr>";
+            "</label></div><br>";
         } else {
           currendVideoSave.innerText = response.data;
         }
       });
     });
-    getAllStoredTimestamps();
   } else {
     if (tabs[0].url.includes("://www.youtube.com")) {
       currendVideoSave.innerText = "Play a Video";
@@ -50,23 +53,26 @@ function deleteTime(id) {
 function getAllStoredTimestamps() {
   chrome.storage.local.get(null).then((data) => {
     Object.entries(data).forEach(([id, details]) => {
-      let articleID = id + "125";
-      itemsContainer.innerHTML += `<article class=${"item"} id=${articleID}><div>
-        <label class=${"title"}>${
-        details.title
-      }</label> <br> <label class=${"stamp"}> ${Math.floor(
-        details.currentVideoTime / 60
-      )} : ${(details.currentVideoTime % 60).toFixed(
-        0
-      )}</label> </div><div><button class=${"deleteBtn"} id=${id}><svg viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" stroke="#000000"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <path d="M10 11V17" stroke="#ffffff" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path> <path d="M14 11V17" stroke="#ffffff" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path> <path d="M4 7H20" stroke="#ffffff" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path> <path d="M6 7H12H18V18C18 19.6569 16.6569 21 15 21H9C7.34315 21 6 19.6569 6 18V7Z" stroke="#ffffff" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path> <path d="M9 5C9 3.89543 9.89543 3 11 3H13C14.1046 3 15 3.89543 15 5V7H9V5Z" stroke="#ffffff" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path> </g></svg></button></div></article>`;
+      itemTempale(id, details);
     });
   });
 }
-itemsContainer.addEventListener("click", (event)=>{
+itemsContainer.addEventListener("click", (event) => {
   let deleteBtn = event.target.closest(".deleteBtn");
-  if(deleteBtn){
+  if (deleteBtn) {
     let itemID = deleteBtn.getAttribute("id");
     deleteTime(itemID);
   }
 });
+
+function itemTempale(id, details) {
+  let articleID = id + "125";
+  itemsContainer.innerHTML += `<article class=${"item"} id=${articleID}><div>
+        <label class=${"title"}>${details.title
+    }</label> <br> <label class=${"stamp"}> ${Math.floor(
+      details.currentVideoTime / 60
+    )} : ${(details.currentVideoTime % 60).toFixed(
+      0
+    )}</label> </div><div><button class=${"deleteBtn"} id=${id}><svg viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" stroke="#000000"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <path d="M10 11V17" stroke="#ffffff" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path> <path d="M14 11V17" stroke="#ffffff" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path> <path d="M4 7H20" stroke="#ffffff" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path> <path d="M6 7H12H18V18C18 19.6569 16.6569 21 15 21H9C7.34315 21 6 19.6569 6 18V7Z" stroke="#ffffff" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path> <path d="M9 5C9 3.89543 9.89543 3 11 3H13C14.1046 3 15 3.89543 15 5V7H9V5Z" stroke="#ffffff" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path> </g></svg></button></div></article>`;
+}
 
